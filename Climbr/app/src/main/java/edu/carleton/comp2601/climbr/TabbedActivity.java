67d67f1d19e93395af0d2aba1475c7dcbe1a1912
Esti Tweg <estictweg@hotmail.com>
2017-03-04 16:19:58 -0500

    Set up find belayer fragment

--- Climbr/app/src/main/java/edu/carleton/comp2601/climbr/TabbedActivity.java
+++ Climbr/app/src/main/java/edu/carleton/comp2601/climbr/TabbedActivity.java
@@ -2,6 +2,7 @@ package edu.carleton.comp2601.climbr;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -39,7 +40,6 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import static edu.carleton.comp2601.climbr.R.id.map;
 
 public class TabbedActivity extends AppCompatActivity {
-public class TabbedActivity extends AppCompatActivity implements FindBelayerFragment.OnFragmentInteractionListener {
 
     static TabbedActivity instance;
 
@@ -231,78 +231,34 @@ public class TabbedActivity extends AppCompatActivity implements FindBelayerFrag
     }
 
 
-    /*public static class MapViewFragment extends Fragment {
 
-        MapView mMapView;
-        private GoogleMap googleMap;
+    public static class FindBelayerFragment extends Fragment {
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.activity_maps, container, false);
-
-            mMapView = (MapView) rootView.findViewById(R.id.mapView);
-            mMapView.onCreate(savedInstanceState);
-
-            mMapView.onResume(); // needed to get the map to display immediately
-
-            try {
-                MapsInitializer.initialize(getActivity().getApplicationContext());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            mMapView.getMapAsync(new OnMapReadyCallback() {
-                @Override
-                public void onMapReady(GoogleMap mMap) {
-                    googleMap = mMap;
-
-                    // For showing a move to my location button
-                    googleMap.setMyLocationEnabled(true);
-
-                    // For dropping a marker at a point on the Map
-                    LatLng sydney = new LatLng(-34, 151);
-                    googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
-
-                    // For zooming automatically to the location of the marker
-                    CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
-                    googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-                }
-            });
-
-            return rootView;
-        }
-
-        @Override
-        public void onResume() {
-            super.onResume();
-            mMapView.onResume();
-        }
-
-        @Override
-        public void onPause() {
-            super.onPause();
-            mMapView.onPause();
-        }
-
-        @Override
-        public void onDestroy() {
-            super.onDestroy();
-            mMapView.onDestroy();
-        }
-
-        @Override
-        public void onLowMemory() {
-            super.onLowMemory();
-            mMapView.onLowMemory();
-        }
-    }*/
-
-    public static class FindBelayerFragment extends Fragment{
+        public FindBelayerFragment() {  }
 
 
         public static FindBelayerFragment newInstance() {
             FindBelayerFragment fragment = new FindBelayerFragment();
+            Bundle args = new Bundle();
+
+
+            fragment.setArguments(args);
             return fragment;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.find_belayer_fragment, container, false);
+
+
+            return rootView;
         }
     }
 
@@ -375,11 +331,6 @@ public class TabbedActivity extends AppCompatActivity implements FindBelayerFrag
         @Override
         public int getCount() {
             return 5;
-        }
-
-        @Override
-        public void onFragmentInteraction(Uri uri){
-            //you can leave it empty
         }
 
 
