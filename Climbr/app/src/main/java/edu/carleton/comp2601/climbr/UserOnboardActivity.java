2a65057e836d13870bd60b2e746215138b9370bd
Olivia Perryman <Olivia@perryman-mac.local>
2017-04-01 15:43:54 -0400

    removed mongo code

--- Climbr/app/src/main/java/edu/carleton/comp2601/climbr/UserOnboardActivity.java
+++ Climbr/app/src/main/java/edu/carleton/comp2601/climbr/UserOnboardActivity.java
@@ -4,17 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoException;
-import com.mongodb.WriteConcern;
 
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
-import com.mongodb.DBCursor;
-
-import com.mongodb.ServerAddress;
 import java.util.Arrays;
 
 public class UserOnboardActivity extends AppCompatActivity {
@@ -30,42 +20,8 @@ public class UserOnboardActivity extends AppCompatActivity {
 
         button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
-                //connect to mongo db
 
-                try{
 
-                    // To connect to mongodb server
-                    MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
-
-                    // Now connect to your databases
-                    DB db = mongoClient.getDB( "findr" );
-                    System.out.println("Connect to database successfully");
-
-                    DBCollection coll = db.getCollection("users");
-                    System.out.println("Collection users selected successfully");
-
-                    DBCursor cursor = coll.find();
-
-                    while (cursor.hasNext()) {
-                        DBObject updateDocument = cursor.next();
-                        updateDocument.put("likes","200");
-                        coll.update(null,updateDocument,true,false);//TODO???
-                    }
-
-                    System.out.println("Document updated successfully");
-                    cursor = coll.find();
-
-                    int i = 1;
-
-                    while (cursor.hasNext()) {
-                        System.out.println("Updated Document: "+i);
-                        System.out.println(cursor.next());
-                        i++;
-                    }
-
-                }catch(Exception e){
-                    System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-                }
 
 
 
